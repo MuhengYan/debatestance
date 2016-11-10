31e63ffa179e410fadce617c14a222f867ce1d8e
MuhengYan <muy6@pitt.edu>
2016-11-10 01:17:32 -0500

    to count how many tweets under specific catagory

--- filterTweets.py
+++ filterTweets.py
@@ -10,14 +10,17 @@ hashtagList = ["#trump2016", "#makeamericagreatagain",
                "#imwithher", "#hillary2016"]
 
 hashtagTrumpFavor = ["#trump2016", "#makeamericagreatagain"]
-
 hashtagClintonFavor = ["#imwithher", "#hillary2016"]
-
 hashtagTrumpAgainst = ["#nevertrump", "#dumptrump"]
-
 hashtagClintonAgainst = ["#dropouthillary", "#neverhillary"]
-
-
+ht1 = ["#trump2016"]
+ht2 = ["#makeamericagreatagain"]
+ht3 = ["#imwithher"]
+ht4 = ["#hillary2016"]
+ht5 = ["#nevertrump"]
+ht6 = ["#dumptrump"]
+ht7 = ["#dropouthillary"]
+ht8 = ["#neverhillary"]
 def detectTargetHashtags(tweet):
     text = tweet.get("text")
     hashtagList = ["#trump2016", "#makeamericagreatagain",
@@ -31,25 +34,49 @@ def detectTargetHashtags(tweet):
 
 def getTargetStance(tweet):
     tokens = tweet.get("tokens")
-    fC = 0
-    fT = 0
-    aC = 0
-    aT = 0
+    h1 = 0
+    h2 = 0
+    h3 = 0
+    h4 = 0
+    h5 = 0
+    h6 = 0
+    h7 = 0
+    h8 = 0
     for token in tokens:
-        if token in hashtagClintonFavor:
-            fC += 1
-        if token in hashtagTrumpFavor:
-            fT += 1
-        if token in hashtagClintonAgainst:
-            aC += 1
-        if token in hashtagTrumpAgainst:
-            aT += 1
+        if token in ht1:
+            h1 += 1
+        if token in ht2:
+            h2 += 1
+        if token in ht3:
+            h3 += 1
+        if token in ht4:
+            h4 += 1
+        if token in ht5:
+            h5 += 1
+        if token in ht6:
+            h6 += 1
+        if token in ht7:
+            h7 += 1
+        if token in ht8:
+            h8 += 1
+    fC = h1 + h2
+    fT = h3 + h4
+    aC = h5 + h6
+    aT = h7 + h8
     _tokens = removeHashtags(tokens)
     tweet["tokens"] = _tokens
     tweet["favorClinton"] = fC
     tweet["favorTrump"] = fT
     tweet["againstClinton"] = aC
     tweet["againstTrump"] = aT
+    tweet["ht1"] = h1
+    tweet["ht2"] = h2
+    tweet["ht3"] = h3
+    tweet["ht4"] = h4
+    tweet["ht5"] = h5
+    tweet["ht6"] = h6
+    tweet["ht7"] = h7
+    tweet["ht8"] = h8
     return tweet
 
 def purgeTweets(tweet):
